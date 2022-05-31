a9777050a2b3fb67d9e051d20cb40d8e0462bdd2
Ivan Ivanovski <102607352+ivan-ivanovski@users.noreply.github.com>
2022-05-31 23:09:03 +0200

    Update find_max_and_min_element.c

--- arrays/find_max_and_min_element.c
+++ arrays/find_max_and_min_element.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-
+#include<limits.h>
 int main(){
   int array[1000]; //declation of array with size of 1000 elements
   int n, i;//declaration of variables, n for number of elements that array will contain, and i is like index, etc, indexing element
-  printf("Array Max and Min Number");
+  int max = INT_MAX, positionMax=0, min = INT_MIN, positionMin = 0;
+  printf("Array Max and Min Number\n");
   printf("Enter a number of elements for array [n] => ");
   scanf("%d", &n);
   for(i = 0; i < n; i++){
@@ -12,6 +13,20 @@ int main(){
   }
   printf("Printing array: \n");
   for(i = 0; i < n; i++){
-    printf("%d ", arr[i]);
+    printf("%d ", array[i]);
   }
+  printf("\n");
+  for(i = 0; i < n; i++){
+      if(array[i] < max){
+        max = array[i];
+        positionMax=i;
+      }
+      if(array[i] > min){
+        min = array[i];
+        positionMin=i;
+      }
+  }
+  printf("Min and max element in array is: min[%d] on position %d, max[%d] on position %d. \n",max,positionMax,min,positionMin);
+  return 0;
+  
 }
